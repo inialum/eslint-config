0bcea78cd2c46f16d16b1f9a0184eb276c6c9e5b
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-04-07 02:08:08 +0900

    feat: Add eslint-plugin-react-refresh fore React preset

--- src/react.js
+++ src/react.js
@@ -8,4 +8,11 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:storybook/recommended',
   ],
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 }
